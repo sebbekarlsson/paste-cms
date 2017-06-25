21839e4936f1bf13d508b602bbd0a0ac16a93680
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2017-06-25 19:14:18 +0200

    added create new page route

--- foliumer/views/admin.py
+++ foliumer/views/admin.py
@@ -7,3 +7,11 @@ bp = Blueprint(__name__, __name__, template_folder='templates',
 @bp.route('/')
 def show():
     return render_template('admin/index.html')
+
+@bp.route('/page/<page_id>', methods=['POST', 'GET'])
+def show_page(page_id):
+    if page_id is not 'new':
+        # get existing page
+        pass
+
+    return render_template('admin/page.html')
