14414aef0c896814242a090baeb84bf405734895
Sebastian Karlsson <sebastian.karlsson@knowit.se>
2017-06-30 14:20:33 +0200

    fixed the setup.js a little bit

--- foliumer/static/js/setup.js
+++ foliumer/static/js/setup.js
@@ -6,17 +6,21 @@ var Setup = function(elem) {
     var _this = this;
 
     _this.db = {};
-    
+
     _this.elem = elem;
     _this.steps = elem.querySelectorAll('.step-by-step-step');
 
+    /**
+     * Initializer
+     *
+     * Runs at instantiation.
+     */
     _this.init = function () {
-
         for (var i = 0; i < _this.steps.length; i++) {
             var step = _this.steps[i];
             var step_prev_btn = step.querySelector('[data-type="prev"]');
             var step_next_btn = step.querySelector('[data-type="next"]');
-            
+
             if (step_prev_btn != null)
                 step_prev_btn.addEventListener('click', function(e) { _this.previous_step(); });
 
@@ -28,14 +32,22 @@ var Setup = function(elem) {
         } 
     };
 
+    /**
+     * Will start the setup.
+     */
     _this.start = function() {
         _this.activate_step(_this.steps[0]);
     };
 
+    /**
+     * Get the ID of the current step.
+     *
+     * @return Int | null
+     */
     _this.get_current_step_id = function () {
         for (var i = 0; i < _this.steps.length; i++) {
             var step = _this.steps[i];
-            
+
             if (step.getAttribute('data-active') == '1')
                 return parseInt(step.getAttribute('data-step-id'));
         }
@@ -43,6 +55,9 @@ var Setup = function(elem) {
         return null;
     };
 
+    /**
+     * Make the setup go to the next step.
+     */
     _this.next_step = function () {
         var current_id = _this.get_current_step_id();
 
@@ -59,9 +74,13 @@ var Setup = function(elem) {
 
         if (typeof errors == 'undefined')
             errors = [];
-        
+
         if (errors.length > 0) {
             for (var i = 0; i < errors.length; i++) {
+                /* Making the error handler none-proprietary by
+                 * making it possible for other developers to create a custom
+                 * error handler.
+                 */
                 if (typeof _this.handle_error != 'undefined') {
                     if (_this.handle_error != null && _this.handle_error) {
                         _this.handle_error(errors[i]);
@@ -75,6 +94,10 @@ var Setup = function(elem) {
         var next_id = current_id + 1;
 
         if (next_id >= _this.steps.length) {
+            /* Making the finish action none-proprietary by
+             * making it possible for other developers to create custom
+             * finish actions.
+             */
             if (typeof _this.handle_finish != 'undefined') {
                 if (_this.handle_finish != null && _this.handle_finish) {
                     _this.handle_finish(_this.steps[current_id]);
@@ -86,6 +109,9 @@ var Setup = function(elem) {
         }
     };
 
+    /**
+     * Make the setup go to the previous step.
+     */
     _this.previous_step = function () {
         var current_id = _this.get_current_step_id();
 
@@ -96,18 +122,18 @@ var Setup = function(elem) {
         _this.activate_step(_this.steps[prev_id]);
     };
 
+    /**
+     * Activate a single step,
+     * will deactivate all other steps.
+     */
     _this.activate_step = function(step) {
         for (var i = 0; i < _this.steps.length; i++) {
             var _step = _this.steps[i];
 
             if (_step == step)
                 continue;
-            
-            if (_step.className.indexOf(__fade_in) > -1) {
-                _step.className = _step.className.replace(__fade_in, '');
-                _step.className += __fade_out;
-            }
-            _step.setAttribute('data-active', '0');
+
+            _this.deactivate_step(_step);
         }
 
         step.className = step.className.replace(__fade_out, '');
@@ -115,9 +141,14 @@ var Setup = function(elem) {
         step.setAttribute('data-active', '1');
     };
 
+    /**
+     * Deactivate a single step.
+     */
     _this.deactivate_step = function(step) {
-        step.className = step.className.replace(__fade_in, '');
-        step.className += __fade_out;
+        if (step.className.indexOf(__fade_in) > -1) {
+            step.className = step.className.replace(__fade_in, '');
+            step.className += __fade_out;
+        }
         step.setAttribute('data-active', '0');
     }
 
