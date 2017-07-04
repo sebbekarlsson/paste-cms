9822ad97cdfd4395aafbc66016852c0d0a747e0f
Sebastian Karlsson <sebastian.karlsson@knowit.se>
2017-07-04 14:37:13 +0200

    added more authentication for password and username

--- foliumer/views/api.py
+++ foliumer/views/api.py
@@ -49,6 +49,12 @@ def show():
         if admin['password'] != admin['password_confirm']:
             return jsonify({'error': 'Passwords does not match!'}), 400
 
+        if len(admin['username']) < 3:
+            return jsonify({'error': 'Username needs to be at least 3 characters'})
+
+        if len(admin['password']) < 3:
+            return jsonify({'error': 'Password needs to be at least 3 characters'})
+
         user = User(username=admin['username'], password=admin['password'])
         option = Option(key='cms_installed', value=True)
 
